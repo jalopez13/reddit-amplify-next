e41e9d029b9e06894a19229dc2f309643bc4ffe7
Javier Lopez <jalopez13@gmail.com>
2022-01-12 21:27:54 -0800

    Added amplify and tailwind css colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          default: "#0d1218",
+          paper: "#1b2330",
+        },
+        primary: {
+          main: "rgba(2, 106, 167, 1)",
+          contrastText: "#fff",
+        },
+        secondary: {
+          main: "rgba(78, 151, 194, 1)",
+          contrastText: "#fff",
+        },
+        error: {
+          main: "#f44336",
+          contrastText: "#fff",
+        },
+        text: {
+          primary: "#fff",
+          secondary: "rgba(255, 255, 255, 0.7)",
+          disabled: "rgba(255, 255, 255, 0.38)",
+          hint: "rgba(255, 255, 255, 0.38)",
+        },
+      },
+    },
   },
   plugins: [],
 };
